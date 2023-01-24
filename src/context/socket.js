2dfd28cb848c4aae360fda715369506095fbd32b
Luka Levac <luka.levac@gmail.com>
2023-01-24 15:33:01 +0000

    config added to socket

--- src/context/socket.js
+++ src/context/socket.js
@@ -1,6 +1,7 @@
 import React, {useState, useContext, useCallback} from 'react'
 import io from "socket.io-client";
+import config from "../config.json";
 
 
-export const socket = io.connect('localhost:8000');
+export const socket = io.connect(config.server);
 export const SocketContext = React.createContext();
